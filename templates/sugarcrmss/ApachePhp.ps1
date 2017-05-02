f569dff02ddceff701e2bd8d543c147eb6e5908c
Yuriy Pankratyev <yuriy.pankratyev@gmail.com>
2017-05-02 20:52:23 +0300

    added httpd unzip

--- templates/sugarcrmss/ApachePhp.ps1
+++ templates/sugarcrmss/ApachePhp.ps1
@@ -14,7 +14,34 @@ Configuration InstallApachePhp
 	        }
 	        GetScript = {@{Result = "DownloadVisualCpp"}}
 	    }
-	   
+		Package InstallVisualCpp
+	    {
+	        Ensure = "Present"  
+	        Path  = "C:\WindowsAzure\vc_redist.x64.exe"
+	        Name = "Microsoft Visual C++ 2015 Redistributable Update 3"
+	        ProductId = "{50A2BC33-C9CD-3BF1-A8FF-53C10A0B183C}"
+	        Arguments = "/install /passive /quiet /norestart"
+	        DependsOn = "[Script]DownloadVisualCpp"
+	    }
+	    Script DownloadApacheHttpd
+	    {
+	        TestScript = {
+	            Test-Path "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
+	        }
+	        SetScript ={
+	            $source = "http://www.apachelounge.com/download/VC14/binaries/httpd-2.4.25-win64-VC14.zip"
+	            $dest = "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
+	            Invoke-WebRequest $source -OutFile $dest
+	        }
+	        GetScript = {@{Result = "DownloadApacheHttpd"}}
+	        DependsOn = "[Package]InstallVisualCpp"
+	    }
+	    Archive UnzipApacheHttpd {
+		    Ensure = "Present"  
+		    Path = "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
+		    Destination = "C:\Apache24"
+		    DependsOn = "[Script]DownloadApacheHttpd"
+		} 
 	    
 
 	}
