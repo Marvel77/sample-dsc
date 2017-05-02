a34cf6dec9856a0bfd855b3de074576824031ae8
Yuriy Pankratyev <yuriy.pankratyev@gmail.com>
2017-05-02 20:32:39 +0300

    added VC++ 2015 installation

--- templates/sugarcrmss/ApachePhp.ps1
+++ templates/sugarcrmss/ApachePhp.ps1
@@ -5,24 +5,16 @@ Configuration InstallApachePhp
 		Script DownloadVisualCpp
 	    {
 	        TestScript = {
-	            Test-Path "C:\WindowsAzure\vcredist_x64.exe"
+	            Test-Path "C:\WindowsAzure\vc_redist.x64.exe"
 	        }
 	        SetScript ={
-	            $source = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
-	            $dest = "C:\WindowsAzure\vcredist_x64.exe"
+	            $source = "https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe"
+	            $dest = "C:\WindowsAzure\vc_redist.x64.exe"
 	            Invoke-WebRequest $source -OutFile $dest
 	        }
 	        GetScript = {@{Result = "DownloadVisualCpp"}}
 	    }
-	    Package InstallVisualCpp
-	    {
-	        Ensure = "Present"  
-	        Path  = "C:\WindowsAzure\vcredist_x64.exe"
-	        Name = "Microsoft Visual C++ 2012 Redistributable (x64) - 11.0.61030"
-	        ProductId = "{CF2BEA3C-26EA-32F8-AA9B-331F7E34BA97}"
-	        Arguments = "/install /passive /quiet /norestart"
-	        DependsOn = "[Script]DownloadVisualCpp"
-	    }
+	   
 	    
 
 	}
