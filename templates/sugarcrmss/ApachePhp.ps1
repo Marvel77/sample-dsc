ad5c200c243ae0cc04b9d6eb41af0ccfcecd9018
Yuriy Pankratyev <yuriy.pankratyev@gmail.com>
2017-05-02 21:09:39 +0300

    added httpd VC11

--- templates/sugarcrmss/ApachePhp.ps1
+++ templates/sugarcrmss/ApachePhp.ps1
@@ -5,32 +5,32 @@ Configuration InstallApachePhp
 		Script DownloadVisualCpp
 	    {
 	        TestScript = {
-	            Test-Path "C:\WindowsAzure\vc_redist.x64.exe"
+	            Test-Path "C:\WindowsAzure\vcredist_x64.exe"
 	        }
 	        SetScript ={
-	            $source = "https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe"
-	            $dest = "C:\WindowsAzure\vc_redist.x64.exe"
+	            $source = "https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe"
+	            $dest = "C:\WindowsAzure\vcredist_x64.exe"
 	            Invoke-WebRequest $source -OutFile $dest
 	        }
 	        GetScript = {@{Result = "DownloadVisualCpp"}}
 	    }
-		Package InstallVisualCpp
+	    Package InstallVisualCpp
 	    {
 	        Ensure = "Present"  
-	        Path  = "C:\WindowsAzure\vc_redist.x64.exe"
-	        Name = "Microsoft Visual C++ 2015 Redistributable Update 3"
-	        ProductId = "{50A2BC33-C9CD-3BF1-A8FF-53C10A0B183C}"
+	        Path  = "C:\WindowsAzure\vcredist_x64.exe"
+	        Name = "Microsoft Visual C++ 2012 Redistributable (x64) - 11.0.61030"
+	        ProductId = "{CF2BEA3C-26EA-32F8-AA9B-331F7E34BA97}"
 	        Arguments = "/install /passive /quiet /norestart"
 	        DependsOn = "[Script]DownloadVisualCpp"
 	    }
 	    Script DownloadApacheHttpd
 	    {
 	        TestScript = {
-	            Test-Path "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
+	            Test-Path "C:\WindowsAzure\httpd-2.4.25-win64-VC11.zip"
 	        }
 	        SetScript ={
-	            $source = "http://www.apachelounge.com/download/VC14/binaries/httpd-2.4.25-win64-VC14.zip"
-	            $dest = "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
+	            $source = "http://www.apachelounge.com/download/VC11/binaries/httpd-2.4.25-win64-VC11.zip"
+	            $dest = "C:\WindowsAzure\httpd-2.4.25-win64-VC11.zip"
 	            Invoke-WebRequest $source -OutFile $dest
 	        }
 	        GetScript = {@{Result = "DownloadApacheHttpd"}}
@@ -38,8 +38,8 @@ Configuration InstallApachePhp
 	    }
 	    Archive UnzipApacheHttpd {
 		    Ensure = "Present"  
-		    Path = "C:\WindowsAzure\httpd-2.4.25-win64-VC14.zip"
-		    Destination = "C:\Apache24"
+		    Path = "C:\WindowsAzure\httpd-2.4.25-win64-VC11.zip"
+		    Destination = "C:\"
 		    DependsOn = "[Script]DownloadApacheHttpd"
 		} 
 	    
